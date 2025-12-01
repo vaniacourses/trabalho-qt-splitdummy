a2ee2ab78352e05d682e704b04c495fbb880271e
Henrique Santana <hsantana.sousass@gmail.com>
2025-12-01 11:59:09 -0300

    adiciona teste de sistema para autenticação

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,13 +1,24 @@
 # Database Cleaner configuration
 RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
   end
 end
--- spec/system/user_auth_spec.rb
+++ spec/system/user_auth_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'User Registration', type: :system do
+RSpec.describe 'User Auth', type: :system do
   it 'permite que um visitante crie uma conta', js: true do
     visit '/'
 
@@ -15,6 +15,19 @@ RSpec.describe 'User Registration', type: :system do
     fill_in 'Senha', with: 'senhasegura', match: :first
     fill_in 'Confirmar Senha', with: 'senhasegura'
     click_button 'Registrar'
+
+    expect(page).to have_content('Bem-vindo, Fulano!')
+  end
+
+  it 'permite que um usuário faça login', js: true do
+    create(:user, email: 'fulano1@email.com', name: 'Fulano')
+
+    visit '/'
+
+    fill_in 'Email', with: 'fulano1@email.com'
+    fill_in 'Senha', with: 'password123'
+    page.save_screenshot('screenshot.png')
+    click_button 'Entrar'
 
     expect(page).to have_content('Bem-vindo, Fulano!')
   end
